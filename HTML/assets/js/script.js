36c9b4d69a392c5d82a5d8265e24c8b3eb192091
viniciussti <viniciussantos8@hotmail.com>
2021-07-21 11:52:49 -0300

    Implementando verificações no formulário e zoom no mapa.

--- HTML/assets/js/script.js
+++ HTML/assets/js/script.js
@@ -1,6 +1,10 @@
 let nome = document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -14,6 +18,7 @@ function validaNome() {
     } else {
         txt.innerHTML = 'Nome Válido!'
         txt.style.color = 'green'
+        nomeOk = true
     }
 }
 function validaEmail() {
@@ -25,6 +30,37 @@ function validaEmail() {
     } else {
         txtEmail.innerHTML = 'E-mail válido!'
         txtEmail.style.color = 'green'
-        
+        emailOk = true
     }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 300) {
+        txtAssunto.innerHTML = 'Limite de 300 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha o formulário corretamente!')
+    }
+}
+
+function mapaZoom(){
+    mapa.style.width = '500px'
+    mapa.style.height = '450px'
+}
+
+function mapaNormal(){
+    mapa.style.width = '400px'
+    mapa.style.height = '350px'
 }
